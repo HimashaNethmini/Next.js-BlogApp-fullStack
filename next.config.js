5a7523c07c96a95a9025f2020523a9f9e2e3b23d
HimashaNethmini <himashanethmini05@gmail.com>
2024-01-05 13:28:50 +0530

    configure the path of pexel website to use photos url in next js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images:{
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "images.pexel.com"
+            }
+        ]
+    }
+}
+  
+// set the path of pexel website to use photos url in next js
 module.exports = nextConfig
