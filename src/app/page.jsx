9ec05bf99e62d1e3005c37cc59af60d53b89be32
HimashaNethmini <himashanethmini05@gmail.com>
2024-01-05 13:39:19 +0530

    completed the homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div> Hi </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          We believe in flexible and precise ideas that naviagtes
+          Our best consulting & finance solution provider.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
